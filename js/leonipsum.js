d7f116b6de2ecf473fecf7d27546862431996515
ShaunYearStrong <shaun@shaundillon.info>
2014-06-16 23:05:41 +0100

    Added algorithm, cleaned JSON

--- js/leonipsum.js
+++ js/leonipsum.js
@@ -1,29 +1,41 @@
-var quotes = jQuery.ajax("/js/quote.json");
+var quotes;
+
+//Fisher-Yates shuffle
+function shuffle(quotes) {
+
+    var m = quotes.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = quotes[m];
+    quotes[m] = quotes[i];
+    quotes[i] = t;
+}
+
+return quotes;
+}
+
 
 function getQuotes(p) {
 
-    var leon = [];
+    var shuffled = shuffle(quotes);
+    console.log(shuffled);
 
-    for (int i = quotes.Length - 1; i > 0; i--)
-    {
-        int n = rand.Next(i + 1);
-        Swap(ref quotes[i], ref quotes[n]);
-    }
+    //leon = leon.join("\n");
 
-    for (var i = 0; i <= p; i++) {
-        leon.push(quotes[i]);
-    }
-
-    leon = leon.join("\n");
-
-    return leon;
+    //return leon;
 }
 
 
 $(document).ready(function() {
 
     $(".okay__button").click(function(){
-        var p = $(".quantity__buttons input").value;
+        var p = $(".quantity__buttons input").val();
         console.log(p);
         var leon = getQuotes(p);
 
@@ -31,4 +43,9 @@ $(document).ready(function() {
     });
 });
 
-
+$(document).ready(function() {
+    $.getJSON('js/quotes.json', function(data) {
+     quotes = data;
+     console.log(quotes);
+ });
+})
