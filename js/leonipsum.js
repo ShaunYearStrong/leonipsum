44333ad20807cd44ea4256df23f5624cdc9ae55e
ShaunYearStrong <shaun@shaundillon.info>
2014-06-23 16:51:41 +0100

    adding JS and JSON quotes file

--- js/leonipsum.js
+++ js/leonipsum.js
@@ -1,51 +1,71 @@
-var quotes;
+var quotes,
+    p = parseInt($(".quantity__buttons input").attr("value"));
 
-//Fisher-Yates shuffle
-function shuffle(quotes) {
+/** shuffle
 
-    var m = quotes.length, t, i;
+    Shuffle an array. 
 
-  // While there remain elements to shuffle…
-  while (m) {
+    @see http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_.22inside-out.22_algorithm
 
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = quotes[m];
-    quotes[m] = quotes[i];
-    quotes[i] = t;
-}
-
-return quotes;
+    @param {Array} array Array to shuffle.
+    @param {Object} rng Optional RNG. Defaults to Math.
+    @return {Array} The original array, shuffled.
+*/
+function shuffle (array, rng) {
+  var i = array.length, j, swap;
+  if (!rng) rng = Math;
+  while (--i) {
+    j = rng.random() * (i + 1) | 0;
+    swap = array[i];
+    array[i] = array[j];
+    array[j] = swap;
+  }
+  console.log(array);
+  return array;
 }
 
 
 function getQuotes(p) {
 
-    var shuffled = shuffle(quotes);
-    console.log(shuffled);
+    var leon = [],
+        shuffled = shuffle(quotes);
 
-    //leon = leon.join("\n");
+    for (i = 0; i < parseInt(p); i++) {
+      leon.push('<p>' + quotes[i] + '</p>');
+    }
 
-    //return leon;
+    return leon;
 }
 
 
 $(document).ready(function() {
 
-    $(".okay__button").click(function(){
-        var p = $(".quantity__buttons input").val();
-        console.log(p);
+    $(".quantity--plus").click(function() {
+      p++
+      $(".quantity__buttons input").attr("value", p);
+    });
+
+    $(".quantity--minus").click(function() {
+      p--
+      $(".quantity__buttons input").attr("value", p);
+    });
+
+    //Get the quotes
+    $.getJSON('js/quotes.json', function(data) {
+         quotes = data.leon;
+         console.log(quotes);
+    });
+
+
+
+    // When the okay button is clicked...
+    $(".okay__button").click(function() {
+        // Get the number of required paragraphs
+        p = parseInt($(".quantity__buttons input").attr("value"))
+        // Send 
         var leon = getQuotes(p);
 
         $('.generate__text').empty().html(leon);
     });
-});
 
-$(document).ready(function() {
-    $.getJSON('js/quotes.json', function(data) {
-     quotes = data;
-     console.log(quotes);
- });
-})
+});
